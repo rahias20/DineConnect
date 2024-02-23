f2c5b98e4bb7c4525920b88671e24b1476d4b277
rahias20 <r.mohammad.20@abdn.ac.uk>
2024-02-23 17:17:56 +0000

    FIREBASE CONNECTED

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:dine_connect/firebase_options.dart';
 import 'package:dine_connect/pages/login_page.dart';
 import 'package:dine_connect/pages/register_page.dart';
 import 'package:dine_connect/themes/theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ChangeNotifierProvider(
     create: (context) => ThemeProvider(),
     child: const MyApp(),
