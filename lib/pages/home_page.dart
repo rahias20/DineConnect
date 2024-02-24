d927dbd53be65009523857f272ccfe0060411fcc
rahias20 <r.mohammad.20@abdn.ac.uk>
2024-02-24 21:25:37 +0000

    top bar added to home page with signout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,23 @@
+import 'package:dine_connect/services/authentication/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final AuthService _authService = AuthService();
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text("Home Page"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _authService.signUserOut(),
+          ),
+        ],
+      ),
       body: SafeArea(child: Center(child: Text("Hello World!"))),
     );
   }
