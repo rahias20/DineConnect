b360c27dc41be65a0b92e2fa89e28de2c1219814
rahias20 <r.mohammad.20@abdn.ac.uk>
2024-02-25 15:53:01 +0000

    pixel overflow fixed - login/singup page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -18,76 +18,78 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // logo
-              const SizedBox(
-                width: 500,
-                child: AspectRatio(
-                  aspectRatio:
-                      3, // This is the aspect ratio of the container, change it as needed
-                  child: Image(
-                    image: AssetImage('lib/images/logo.png'),
-                    fit: BoxFit
-                        .contain, // This will contain the image within the container's bounds
-                  ),
-                ),
-              ),
-              // welcome back message
-              const SizedBox(height: 25),
-              Text(
-                "Let's create an account for you",
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary, fontSize: 16),
-              ),
-              // email text field
-              const SizedBox(height: 25),
-              MyTextField(
-                  hintText: "Email",
-                  obscureText: false,
-                  controller: _emailController),
-
-              // password text field
-              const SizedBox(height: 25),
-              MyTextField(
-                  hintText: "Password",
-                  obscureText: true,
-                  controller: _passwordController),
-
-              // confirm password field
-              const SizedBox(height: 25),
-              MyTextField(
-                  hintText: "Confirm password",
-                  obscureText: true,
-                  controller: _confirmPasswordController),
-
-              // register button
-              const SizedBox(height: 25),
-              MyButton(text: "Register", onTap: () => register(context)),
-
-              // login
-              const SizedBox(height: 25),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Already a member? ",
-                      style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary)),
-                  GestureDetector(
-                    onTap: onTap,
-                    child: Text(
-                      "Login now",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).colorScheme.primary),
+      body: SingleChildScrollView(
+        child: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // logo
+                const SizedBox(
+                  width: 500,
+                  child: AspectRatio(
+                    aspectRatio:
+                        3, // This is the aspect ratio of the container, change it as needed
+                    child: Image(
+                      image: AssetImage('lib/images/logo.png'),
+                      fit: BoxFit
+                          .contain, // This will contain the image within the container's bounds
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                // welcome back message
+                const SizedBox(height: 25),
+                Text(
+                  "Let's create an account for you",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary, fontSize: 16),
+                ),
+                // email text field
+                const SizedBox(height: 25),
+                MyTextField(
+                    hintText: "Email",
+                    obscureText: false,
+                    controller: _emailController),
+        
+                // password text field
+                const SizedBox(height: 25),
+                MyTextField(
+                    hintText: "Password",
+                    obscureText: true,
+                    controller: _passwordController),
+        
+                // confirm password field
+                const SizedBox(height: 25),
+                MyTextField(
+                    hintText: "Confirm password",
+                    obscureText: true,
+                    controller: _confirmPasswordController),
+        
+                // register button
+                const SizedBox(height: 25),
+                MyButton(text: "Register", onTap: () => register(context)),
+        
+                // login
+                const SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Already a member? ",
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary)),
+                    GestureDetector(
+                      onTap: onTap,
+                      child: Text(
+                        "Login now",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
