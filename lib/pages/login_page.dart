befd2a1cd560f1c7b7c2bf4c9289ffff2f3b0494
rahias20 <r.mohammad.20@abdn.ac.uk>
2024-03-13 13:02:53 +0000

    regular expression added

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -29,6 +29,15 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+    final regex = RegExp(pattern);
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Form(
@@ -77,8 +86,11 @@ class _LoginPageState extends State<LoginPage> {
                       labelText: 'Email',
                       validator: (value) {
                         if (value == null ||
-                            value.isEmpty ||
-                            !value.contains('@')) {
+                            value.isEmpty) {
+                          return 'Please enter an email';
+                        }
+                        // Regular expression pattern for validating email format
+                        if (!regex.hasMatch(value)) {
                           return 'Invalid email';
                         }
                         return null;
