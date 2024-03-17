abbb8115100e83de8d011933fa739ef0c4137126
rahias20 <r.mohammad.20@abdn.ac.uk>
2024-03-17 17:50:52 +0000

    host event page - option added for deleting upcoming events

--- lib/pages/host_page.dart
+++ lib/pages/host_page.dart
@@ -1,4 +1,7 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../models/event.dart';
 import '../services/authentication/auth_service.dart';
@@ -25,23 +28,25 @@ class _HostPageState extends State<HostPage> {
     _fetchCreatedEvents();
   }
 
-Future<void> _fetchCreatedEvents() async {
-  String? uid = _authService.getCurrentUser()?.uid;
-  DateTime now = DateTime.now();
-  if (uid != null) {
-    try {
-      List<Event> events = await _eventService.fetchEventsCreatedByUser(uid);
-      setState(() {
-        _upcomingEvents = events.where((event) => event.eventDate.isAfter(now)).toList();
-        _pastEvents = events.where((event) => event.eventDate.isBefore(now)).toList();
-      });
-    } catch (e) {
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(e.toString())));
+  Future<void> _fetchCreatedEvents() async {
+    String? uid = _authService.getCurrentUser()?.uid;
+    DateTime now = DateTime.now();
+    if (uid != null) {
+      try {
+        List<Event> events = await _eventService.fetchEventsCreatedByUser(uid);
+        setState(() {
+          _upcomingEvents =
+              events.where((event) => event.eventDate.isAfter(now)).toList();
+          _pastEvents =
+              events.where((event) => event.eventDate.isBefore(now)).toList();
+        });
+      } catch (e) {
+        if (!mounted) return;
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(e.toString())));
+      }
     }
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
@@ -55,17 +60,26 @@ Future<void> _fetchCreatedEvents() async {
         appBar: AppBar(
           title: const Text('Host an Event'),
           backgroundColor: colorScheme.secondary,
-        bottom: const TabBar(tabs: [Tab(text: 'Upcoming Events',), Tab(text: 'Past Events',)]),
+          bottom: const TabBar(tabs: [
+            Tab(
+              text: 'Upcoming Events',
+            ),
+            Tab(
+              text: 'Past Events',
+            )
+          ]),
         ),
-
         body: TabBarView(
           children: [
             Padding(
               padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
               child: Column(
                 children: [
-                  SizedBox(height: screenHeight * 0.02), // dynamic space before the list
-                  Expanded(child: _buildEventsList(_upcomingEvents, screenHeight)),
+                  SizedBox(
+                      height:
+                          screenHeight * 0.02), // dynamic space before the list
+                  Expanded(
+                      child: _buildEventsList(_upcomingEvents, screenHeight, true)),
                 ],
               ),
             ),
@@ -73,8 +87,10 @@ Future<void> _fetchCreatedEvents() async {
               padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
               child: Column(
                 children: [
-                  SizedBox(height: screenHeight * 0.02), // dynamic space before the list
-                  Expanded(child: _buildEventsList(_pastEvents, screenHeight)),
+                  SizedBox(
+                      height:
+                          screenHeight * 0.02), // dynamic space before the list
+                  Expanded(child: _buildEventsList(_pastEvents, screenHeight, false)),
                 ],
               ),
             ),
@@ -88,7 +104,8 @@ Future<void> _fetchCreatedEvents() async {
     );
   }
 
-  Widget _buildEventsList(List<Event> events, double screenHeight) {
+  Widget _buildEventsList(
+      List<Event> events, double screenHeight, bool isUpcoming) {
     return Padding(
       padding: EdgeInsets.only(top: screenHeight * 0.02),
       child: ListView.builder(
@@ -96,9 +113,50 @@ Future<void> _fetchCreatedEvents() async {
         itemBuilder: (BuildContext context, int index) {
           Event event = events[index];
           return Card(
+            elevation: 2,
             child: ListTile(
               title: Text(event.description),
-              subtitle: Text(event.description),
+              subtitle: Text(
+                "${DateFormat('EEE, MMM d, yyyy').format(event.eventDate)} at ${DateFormat('h:mm a').format(event.eventDate)}",
+              ),
+              trailing: isUpcoming
+                  ? IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () async {
+                        // Confirm dialog before deleting
+                        bool confirmDelete = await showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('Delete Event'),
+                              content: const Text(
+                                  'Are you sure you want to delete this event?'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(false),
+                                  child: Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(true),
+                                  child: const Text('Delete',
+                                      style: TextStyle(color: Colors.red)),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+
+                        if (confirmDelete) {
+                          // Perform the delete operation
+                          await _eventService.deleteEvent(event.eventId);
+                          // Refresh the list
+                          _fetchCreatedEvents();
+                        }
+                      },
+                    )
+                  : null, // no trailing button for past events
             ),
           );
         },
