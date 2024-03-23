045c43dab48933c5d8c1b3ea036c4da5ca042fef
rahias20 <r.mohammad.20@abdn.ac.uk>
2024-03-23 16:43:43 +0000

    userprofile model changed to include email and joinedEventsIds array

--- lib/models/user_profile.dart
+++ lib/models/user_profile.dart
@@ -1,35 +1,40 @@
 class UserProfile {
   final String userId;
   final String name;
+  final String userEmail;
   final int age;
   final String bio;
   final String lookingFor;
   final String? imageUrl;
   final String location;
   final List<String> hobbies;
+  final List<String> joinedEventsIds;
 
-  UserProfile({
-    required this.userId,
-    required this.name,
-    required this.age,
-    required this.bio,
-    required this.lookingFor,
-    this.imageUrl,
-    required this.location,
-    required this.hobbies,
-  });
+  UserProfile(
+      {required this.userId,
+      required this.name,
+      required this.userEmail,
+      required this.age,
+      required this.bio,
+      required this.lookingFor,
+      this.imageUrl,
+      required this.location,
+      required this.hobbies,
+      required this.joinedEventsIds});
 
   // convert to a map
   Map<String, dynamic> toMap() {
     return {
       'userId': userId,
       'name': name,
+      'userEmail': userEmail,
       'age': age,
       'bio': bio,
       'lookingFor': lookingFor,
       'imageUrl': imageUrl,
       'location': location,
-      'hobbies': hobbies
+      'hobbies': hobbies,
+      'joinedEventsIds': joinedEventsIds
     };
   }
 
@@ -38,11 +43,14 @@ class UserProfile {
     return UserProfile(
         userId: map['userId'],
         name: map['name'],
+        userEmail: map['userEmail'],
         age: map['age'],
         bio: map['bio'],
         lookingFor: map['lookingFor'],
-        imageUrl: map['imageUrl'] as String?, // cast as String since it can be null
+        imageUrl:
+            map['imageUrl'] as String?, // cast as String since it can be null
         location: map['location'],
-        hobbies: List<String>.from(map['hobbies']));
+        hobbies: List<String>.from(map['hobbies']),
+        joinedEventsIds: List<String>.from(map['joinedEventsIds']));
   }
 }
