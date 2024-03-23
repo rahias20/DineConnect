60b5291ff358ec19dbb4e20f71032050376cddb8
rahias20 <r.mohammad.20@abdn.ac.uk>
2024-03-23 16:37:04 +0000

    chat page added

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: colorScheme.primary,
+      ),
+    );
+  }
+}
