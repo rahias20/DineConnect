072e019399984a21f003844140f0f629d727fa93
rahias20 <>
2024-03-30 18:15:58 +0000

    showing participants profile image with max participants

--- lib/components/event_content.dart
+++ lib/components/event_content.dart
@@ -31,12 +31,15 @@ class _EventContentState extends State<EventContent> {
   final AuthService _authService = AuthService();
   final EventService _eventService = EventService();
   String imageUrl = '';
+  // list to store participant profiles
+  List<UserProfile> _participantsProfiles = [];
 
   @override
   void initState() {
     super.initState();
     _userProfileService = UserProfileService();
     _fetchUserProfile();
+    _fetchParticipantsProfiles();
   }
 
   Future<void> _fetchUserProfile() async {
@@ -56,6 +59,22 @@ class _EventContentState extends State<EventContent> {
     }
   }
 
+  Future<void> _fetchParticipantsProfiles() async {
+    for (var userId in widget.event.participantUserIds) {
+      try {
+        UserProfile? profile =
+            await _userProfileService.fetchUserProfile(userId);
+        if (profile != null) {
+          setState(() => _participantsProfiles.add(profile));
+        }
+      } catch (e) {
+        if (!mounted) return;
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(e.toString())));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
@@ -67,86 +86,136 @@ class _EventContentState extends State<EventContent> {
       appBar: AppBar(
         backgroundColor: colorScheme.primary,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(screenWidth / 7),
-          child: Column(
-            children: [
-              Text(
-                widget.event.description,
-                style: TextStyle(
-                  fontSize: screenHeight * 0.03,
-                  fontWeight: FontWeight.w500,
+      body: _userProfile == null
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(screenWidth / 7),
+                child: Column(
+                  children: [
+                    Text(
+                      widget.event.description,
+                      style: TextStyle(
+                        fontSize: screenHeight * 0.03,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.04),
+                    Row(
+                      children: [
+                        SizedBox(height: screenHeight * 0.04),
+                        Stack(
+                          children: [
+                            CircleAvatar(
+                              radius: screenWidth * 0.05,
+                              backgroundImage: imageUrl.isNotEmpty
+                                  ? NetworkImage(imageUrl)
+                                  : const AssetImage(
+                                          'lib/images/profile_icon.png')
+                                      as ImageProvider,
+                              backgroundColor: Colors.white60,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 10),
+                        const Text(
+                          'Hosted by ',
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                        GestureDetector(
+                          onTap: widget.onHostClicked,
+                          child: Text(
+                            '${_userProfile?.name.split(' ')[0]}',
+                            style: TextStyle(
+                                fontSize: 16.0, color: Colors.red.shade400),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenHeight * 0.04),
+                    Row(
+                      children: [
+                        const Icon(Icons.event),
+                        const SizedBox(width: 15),
+                        Text(
+                          DateFormat('EEE, MMM d yyyy\nh:mm a').format(widget
+                              .event
+                              .eventDate), // Use event.date here formatted
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenHeight * 0.03),
+                    Row(
+                      children: [
+                        const Icon(Icons.location_on_sharp),
+                        const SizedBox(width: 15),
+                        Text(
+                          '${widget.event.addressLine1}\n${widget.event.addressLine2}\n${widget.event.city}\n${widget.event.postcode}', // Use event.location here
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenHeight * 0.04),
+                    Row(
+                      children: [
+                        const Icon(Icons.person),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Expanded(
+                          child: Container(
+                            height: 50,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: _participantsProfiles.length,
+                              itemBuilder: (context, index) {
+                                UserProfile participant =
+                                    _participantsProfiles[index];
+                                return GestureDetector(
+                                  onTap: () {
+                                    Navigator.pushNamed(
+                                        context, '/hostProfilePage',
+                                        arguments: {
+                                          'userProfile': participant,
+                                          'eventId': widget.event.eventId
+                                        });
+                                  },
+                                  child: CircleAvatar(
+                                    backgroundImage: participant
+                                            .imageUrl!.isNotEmpty
+                                        ? NetworkImage(
+                                            participant.imageUrl.toString())
+                                        : const AssetImage(
+                                                'lib/images/profile_icon.png')
+                                            as ImageProvider,
+                                    backgroundColor: Colors.white60,
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Text(
+                          'Maximum ${widget.event.numberOfParticipants} people ',
+                          style: TextStyle(fontSize: 11.0),
+                        ),
+                        Text(
+                          '${widget.event.numberOfParticipants - (widget.event.participantUserIds.length + 1)} MORE SPOT OPEN',
+                          style: TextStyle(
+                              fontSize: 11.0, color: Colors.red.shade400),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: screenHeight * 0.04),
-              Row(
-                children: [
-                  SizedBox(height: screenHeight * 0.04),
-                  Stack(
-                    children: [
-                      CircleAvatar(
-                        radius: screenWidth * 0.05,
-                        backgroundImage: imageUrl.isNotEmpty
-                            ? NetworkImage(imageUrl)
-                            : const AssetImage('lib/images/profile_icon.png')
-                                as ImageProvider,
-                        backgroundColor: Colors.white60,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(width: 10),
-                  const Text(
-                    'Hosted by ',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  GestureDetector(
-                    onTap: widget.onHostClicked,
-                    child: Text(
-                      '${_userProfile?.name.split(' ')[0]}',
-                      style: TextStyle(fontSize: 16.0, color: Colors.red[300]),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: screenHeight * 0.04),
-              Row(
-                children: [
-                  const Icon(Icons.event),
-                  const SizedBox(width: 15),
-                  Text(
-                    DateFormat('EEE, MMM d yyyy\nh:mm a').format(widget
-                        .event.eventDate), // Use event.date here formatted
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              ),
-              SizedBox(height: screenHeight * 0.03),
-              Row(
-                children: [
-                  const Icon(Icons.location_on_sharp),
-                  const SizedBox(width: 15),
-                  Text(
-                    '${widget.event.addressLine1}\n${widget.event.addressLine2}\n${widget.event.city}\n${widget.event.postcode}', // Use event.location here
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              ),
-              SizedBox(height: screenHeight * 0.04),
-              Row(
-                children: [
-                  const Icon(Icons.person),
-                  const SizedBox(width: 15),
-                  Text(
-                    '${widget.event.numberOfParticipants}',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
       bottomNavigationBar: NavbarButton(
         onPressed: widget.navbarButtonPressed,
         buttonText: widget.navbarButtonText,
